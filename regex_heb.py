d35ff1644ce0ff596bbfca481e841df13a5551cd
Elazar Gershuni <elazarg@gmail.com>
2020-06-16 00:06:50 +0300

    kuvnu

--- regex_heb.py
+++ regex_heb.py
@@ -45,5 +45,5 @@ def enumerate_possible_forms(verb):
 HEADER = ('שורש', "ו", "שימוש", "מילה", "סיומת", "בניין", "זמן", "גוף", "מין", "מספר")
 
 if __name__ == '__main__':
-    for x in enumerate_possible_forms('התייקר'):
+    for x in enumerate_possible_forms('כשכוונו'):
         print(x)
