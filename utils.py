0ee423ab475627c4f82c6cf6120ddaee37d07e9d
Elazar Gershuni <elazarg@gmail.com>
2020-07-01 17:48:30 +0300

    batch-validation

--- utils.py
+++ utils.py
@@ -11,81 +11,27 @@ from encoding import class_size, class_name, combined_shape, CLASSES
 
 
 class Stats:
-    def __init__(self, names, runsize):
-        self.runsize = runsize
+    def __init__(self, names):
         self.initial_validated = False
-        self.epoch = 0
         self.names = list(names)
-        self.zero_run()
-
-    def zero_run(self):
         self.running_corrects = {k: 0.0 for k in self.names}
         self.init_unravelled()
         self.running_divisor = 0
         self.running_loss = []
         self.confusion = {k: np.zeros((class_size(k), class_size(k))) for k in self.names}
 
-    def assert_reasonable_initial(self, losses, criterion):
-        if not self.initial_validated:
-            if isinstance(criterion, torch.nn.CrossEntropyLoss):
-                for k in losses:
-                    expected_ce_losses = -np.log(1 / class_size(k))
-                    assert abs(1 - losses[k] / expected_ce_losses) < 0.2
-            self.initial_validated = True
-
-    def epoch_start(self):
-        self.epoch += 1
-
-    def epoch_end(self):
-        self.epoch = 0
-
-    def phase_start(self, phase, batches_in_phase):
-        self.phase = phase
-        self.batches_in_phase = batches_in_phase
-        self.zero_run()
-        self.batch = 0
-
-    def phase_end(self):
-        if self.phase != 'train':
-            self.callback()
-
-        print()
-
-    def batch_start(self):
-        self.batch += 1
-
-    def batch_end(self):
-        if self.phase == 'train' and self.batch % self.runsize == 0:
-            self.callback()
-            self.zero_run()
-
-    def cli(self, mean_loss, accuracies):
-        print("{:2} {:5}/{:5}".format(self.epoch, self.batch, self.batches_in_phase), end=' ')
-        for k, v in accuracies.items():
-            print("{}_acc: {:.3f}".format(class_name(k), v), end=' ')
-        print("Loss: {:.4f}".format(mean_loss), end='\r')
-
-    def wandb(self, mean_loss, accuracies):
-        pref = "train" if self.phase == 'train' else "val"
-        wandb.log({
-            'phase': self.phase,
-            'epoch': self.epoch,
-            'batch': self.batch,
-            f"{pref}/Loss": mean_loss,
-            **{f"{pref}/Accuracy_{class_name(k)}": accuracies[k] for k in accuracies},
-            **{f"{pref}/Confusion_{class_name(k)}": self.confusion[k] for k in self.confusion}
-        })
-
-    def callback(self):
+    def summary(self):
         mean_loss = np.mean(self.running_loss)
         accuracies = {k: v / self.running_divisor
                       for k, v in self.running_corrects.items()}
 
-        self.confusion = {k: v / self.running_divisor
-                          for k, v in self.confusion.items()}
-
-        self.cli(mean_loss, accuracies)
-        self.wandb(mean_loss, accuracies)
+        confusion = {k: v / self.running_divisor
+                     for k, v in self.confusion.items()}
+        return {
+            "Loss": mean_loss,
+            **{f"Accuracy_{class_name(k)}": accuracies[k] for k in accuracies},
+            **{f"Confusion_{class_name(k)}": confusion[k] for k in confusion}
+        }
 
     def update(self, loss, batch_size, d):
         self.running_loss.append(loss)
@@ -114,6 +60,45 @@ class Stats:
             if isinstance(combination, (tuple, list)) and len(combination) > 1:
                 for k in combination:
                     self.running_corrects[k] = 0.0
+
+
+class Once:
+    def __init__(self, f):
+        self.happened = False
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        if not self.happened:
+            self.happened = True
+            self.f(*args, **kwargs)
+
+
+def assert_reasonable_initial(losses, criterion):
+    if isinstance(criterion, torch.nn.CrossEntropyLoss):
+        for k in losses:
+            expected_ce_losses = -np.log(1 / class_size(k))
+            assert abs(1 - losses[k] / expected_ce_losses) < 0.2
+
+
+def cli_log(config, nbatches):
+    print(f"{config['epoch']:2} {config['batch']:5}/{nbatches:5}", end=' ')
+    for k, v in config.items():
+        if 'Accuracy' in k:
+            print(f"{k}: {v:.3f}", end=' ')
+        elif 'Loss' in k:
+            print(f"{k}: {v:.4f}", end=' ')
+    print(end='\r')
+
+
+def log(train: Stats, test: Stats, batch, nbatches, epoch):
+    conf = {
+        'epoch': epoch,
+        'batch': batch,
+        **{f'train/{k}': v for k, v in train.summary().items()},
+        **{f'val/{k}': v for k, v in test.summary().items()}
+    }
+    cli_log(conf, nbatches)
+    wandb.log(conf)
 
 
 def conditional_grad(condition):
